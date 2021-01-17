01b86e4bb1fe4128f6f7e37488f388b8f0e9cc46
e <qingxiu.zhang@zqgame.com>
2021-01-17 22:46:44 +0800

    新增下载加载状态

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './utils/common.less'
 function App() {
   return (
     // <BrowserRouter></BrowserRouter> 路由地址不带#
-    // HashRouter 路由地址带#
+    // HashRouter 路由地址带#  跳转详情页面的传值就不能用路由传递了，要用memroy进行暂存数据，子页面取出来
     <BrowserRouter> 
       <Switch>
         <Route path='/login' component={Login}></Route>
